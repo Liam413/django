a63d5c814901ca2433f41464e37eff5635a02634
diaoyizi <diao.yizi@immomo.com>
2015-02-15 16:47:11 +0800

    model

--- eshop/order/admin.py
+++ eshop/order/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from models import Product
-from models import Ptype
+from models import Product,Ptype,Order,UserProfile
 
 admin.site.register(Product)
 admin.site.register(Ptype)
+admin.site.register(Order)
+admin.site.register(UserProfile)
--- eshop/order/models.py
+++ eshop/order/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
     name = models.CharField('product name', max_length=30)
     price = models.FloatField('price', default=10)
     ptype = models.ForeignKey('Ptype')
+    img   = models.ImageField('img', upload_to="product",max_length=100)
     
     def __unicode__(self):
         return "%s -- %f"% (self.name, self.price)
@@ -15,3 +16,20 @@ class Ptype(models.Model):
     
     def __unicode__(self):
         return "%s"% self.name
+    
+class Order(models.Model):
+    odate   = models.DateTimeField('order date', auto_now=True)
+    product = models.ForeignKey("product")
+    user    = models.ForeignKey('User')
+   
+#create your model here  
+class UserProfile(models.Model):
+    SEX_CHOICES = (
+        ('1', u'男'),
+        ('2', u'女'),
+    )
+    
+    user = models.OneToOneField(User)
+    nickname = models.CharField(u'昵称',max_length=30)
+    sex = models.CharField(u'性别',max_length=1,choices=SEX_CHOICES,default='1')
+    address = models.CharField(u'地址',max_length=100,null=True)
